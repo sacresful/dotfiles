890c6707d2978adebebfa92474479240255eb2f7
sacresful <sacresful@proton.me>
2023-09-10 12:11:34 +0200

    Initial commit

---
+++ .local/bin/xdg-ninja/haskell/lib/AddProgram.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- I do not know haskell, this code is probably shit
+
+module AddProgram where
+
+import           Data.Aeson
+import           Data.Aeson.Encode.Pretty
+import           Data.Aeson.Types
+import qualified Data.ByteString.Lazy     as B
+import           Data.List.Extra
+import           Data.Maybe
+import qualified Data.Text                as T
+import           Data.Text.ANSI
+import           Data.UUID
+import           Data.UUID.V4
+import           GHC.Float                (double2Float)
+import           GHC.Generics
+import           Program
+import           Prompts
+import           System.Console.Haskeline
+import           System.Environment       (getEnv)
+import           System.Exit
+import           System.IO
+import           System.Process
+import           Text.Printf              (printf)
+
+getTemplate :: SupportLevel -> String
+getTemplate Unsupported = "Currently unsupported.\n\n_Relevant issue:_ https://github.com/user/repo/issues/nr\n"
+getTemplate EnvVars = "Export the following environment variables:\n\n```bash\n\n```"
+getTemplate Alias = "Alias PROGRAM to use a custom configuration location:\n\n```bash\nalias PROGRAM=PROGRAM --config \"$XDG_CONFIG_HOME\"/PROGRAM/config\n```\n"
+getTemplate Supported = "Supported since _VERSION_.\n\nYou can move the file to _XDG_CONFIG_HOME/PROGRAM/CONFIG.\n"
+
+getHelp :: SupportLevel -> IO String
+getHelp supportLevel = getInputMarkdown (getTemplate supportLevel)
+
+getSupportLevel :: IO SupportLevel
+getSupportLevel = do
+  movable <- promptBool (blue "Can the file be moved? (y/n) ") (red "Please provide a valid answer.") "y"
+  if movable
+  then do
+    envVars <- promptBool (blue "Do you have to export environment variables? (y/n) ") (red "Please provide a valid answer.") "y"
+    if envVars
+    then return EnvVars
+    else do
+      alias <- promptBool (blue "Do you have to set an alias? (y/n) ") (red "Please provide a valid answer.") "y"
+      if alias
+      then return Alias
+      else return Supported
+  else return Unsupported
+
+getFile :: IO File
+getFile = do
+  path <- getProp (blue "Path to file: ") "$HOME/."
+  supportLevel <- getSupportLevel
+  help <- getHelp supportLevel
+  return File {path = path, supportLevel = supportLevel, help = help}
+
+getFiles :: [File] -> IO [File]
+getFiles files =
+  if Data.List.Extra.null files
+  then do
+    newFile <- getFile
+    getFiles (newFile : files)
+  else do
+    new <- promptBool (green "Add another file? (y/n) ") (red "Please provide a valid answer.") ""
+    if new
+    then do
+      newFile <- getFile
+      getFiles (newFile : files)
+    else return files
+
+getProgram :: IO Program
+getProgram = do
+  name <- printf "%s\n" (T.unpack (bold (cyan "XDG-ninja Configuration Wizard")))
+          >> printf "%s\n" (T.unpack (faint (italic (cyan "First, tell me what program you're creating a configuration for."))))
+          >> getProp (yellow "Program name: ") ""
+  files <- printf "%s\n" (T.unpack (faint (italic (cyan "Alright, now let's configure which files belong to this program."))))
+           >> printf "%s\n" (T.unpack (faint (italic (cyan "I'm going to ask you for the path to the file, please use $HOME instead of ~."))))
+           >> printf "%s\n" (T.unpack (faint (italic (cyan "I'll then ask you whether or not this file can be moved to a different directory."))))
+           >> printf "%s\n" (T.unpack (faint (italic (cyan "Finally, your editor is going to open a markdown document. Enter instructions on moving the file in question, then save and close."))))
+           >> getFiles []
+  return Program {name = T.pack name, files = files}
+
+saveProgram :: IO ()
+saveProgram = do
+  program <- getProgram
+  do_save <- promptBool (green "Save? (y/n) ") (red "Please provide a valid answer.") ""
+  if do_save
+  then save (makeFilename (name program)) program
+  else return ()
---
+++ .local/bin/xdg-ninja/haskell/lib/Program.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Program where
+
+import           Data.Aeson
+import           Data.Aeson.Encode.Pretty
+import qualified Data.ByteString.Lazy     as B
+import qualified Data.Text                as T
+import           GHC.Generics
+import           System.Directory
+
+instance FromJSON File where
+  parseJSON (Object v) = File
+    <$> v .: "path"
+    <*> v .: "movable"
+    <*> v .: "help"
+
+data Program = Program
+  { name  :: T.Text,
+    files :: [File]
+  }
+  deriving (Generic, Show)
+
+instance ToJSON Program where
+  toJSON (Program name files) = object [ "name" .= name, "files" .= files ]
+  toEncoding (Program name files) = pairs ("name" .= name <> "files" .= files)
+instance FromJSON Program
+
+data File = File
+  { path         :: String,
+    supportLevel :: SupportLevel,
+    help         :: String
+  }
+  deriving (Generic, Show)
+
+instance ToJSON File where
+  toJSON (File path supportLevel help) = object [ "path" .= path, "movable" .= supportLevel, "help" .= help ]
+  toEncoding (File path supportLevel help) = pairs ("path" .= path <> "movable" .= supportLevel <> "help" .= help)
+
+data SupportLevel = Unsupported | Alias | EnvVars | Supported
+  deriving (Generic, Show)
+
+instance ToJSON SupportLevel where
+  toJSON Unsupported = toJSON (Bool False)
+  toJSON _           = toJSON (Bool True)
+  toEncoding Unsupported = toEncoding ( Bool False )
+  toEncoding _           = toEncoding ( Bool True )
+instance FromJSON SupportLevel where
+  parseJSON (Bool False) = return Unsupported
+  parseJSON (Bool True)  = return EnvVars
+
+makeFilename :: T.Text -> T.Text
+makeFilename s = T.pack ( "./programs/" ++ T.unpack s ++ ".json" )
+
+save :: T.Text -> Program -> IO ()
+save filename program = do
+  createDirectoryIfMissing False "./programs/"
+  B.writeFile (T.unpack filename) (encodePretty program)
+
+readProgram :: String -> IO ( Maybe Program )
+readProgram filename = do
+    json_data <- B.readFile filename
+    return (decode json_data)
