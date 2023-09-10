10fd16a0218885cd631c090b81e8b5ba9ed14e01
sacresful <sacresful@proton.me>
2023-09-10 12:07:31 +0200

    Initial commit

---
+++ .config/suckless/dwmblocks/config.c
@@ -0,0 +1,19 @@
+#include "config.h"
+
+#include "block.h"
+#include "util.h"
+
+Block blocks[] = {
+//    {"",	       x,     10},	
+//    {"",	       x,      9},	
+//    {"",	       x,      8},	
+    {"sb-updates",     0,      7},	
+    {"sb-disk",	    3600,      6},	
+    {"sb-memory",     10,      5},	
+    {"sb-network",     0,      4},	
+    {"sb-volume",      0,      3},	
+    {"sb-clock",      60,      2},
+    {"sb-battery",    10,      1},	
+};
+
+const unsigned short blockCount = LEN(blocks);
