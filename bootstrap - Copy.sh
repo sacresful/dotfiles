467cf9c989c65a72c124ca6f8aad004146ab5efe
sacresful <57761430+sacresful@users.noreply.github.com>
2024-07-20 23:58:14 +0200

    Add files via upload

--- bootstrap - Copy.sh
+++ bootstrap - Copy.sh
@@ -8,41 +8,178 @@ install () {
 
 # in live cd
 
+pacman -S gptfdisk
+
 while true 
 do
 	read -p "Installation type: normal or encrypted? " INSTALL_TYPE
 	break
 done
 
+select_option() {
+
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()  { printf "$ESC[?25h"; }
+    cursor_blink_off() { printf "$ESC[?25l"; }
+    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+    print_option()     { printf "$2   $1 "; }
+    print_selected()   { printf "$2  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    get_cursor_col()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${COL#*[}; }
+    key_input()         {
+                        local key
+                        IFS= read -rsn1 key 2>/dev/null >&2
+                        if [[ $key = ""      ]]; then echo enter; fi;
+                        if [[ $key = $'\x20' ]]; then echo space; fi;
+                        if [[ $key = "k" ]]; then echo up; fi;
+                        if [[ $key = "j" ]]; then echo down; fi;
+                        if [[ $key = "h" ]]; then echo left; fi;
+                        if [[ $key = "l" ]]; then echo right; fi;
+                        if [[ $key = "a" ]]; then echo all; fi;
+                        if [[ $key = "n" ]]; then echo none; fi;
+                        if [[ $key = $'\x1b' ]]; then
+                            read -rsn2 key
+                            if [[ $key = [A || $key = k ]]; then echo up;    fi;
+                            if [[ $key = [B || $key = j ]]; then echo down;  fi;
+                            if [[ $key = [C || $key = l ]]; then echo right;  fi;
+                            if [[ $key = [D || $key = h ]]; then echo left;  fi;
+                        fi 
+    }
+    print_options_multicol() {
+        # print options by overwriting the last lines
+        local curr_col=$1
+        local curr_row=$2
+        local curr_idx=0
+
+        local idx=0
+        local row=0
+        local col=0
+        
+        curr_idx=$(( $curr_col + $curr_row * $colmax ))
+        
+        for option in "${options[@]}"; do
+
+            row=$(( $idx/$colmax ))
+            col=$(( $idx - $row * $colmax ))
+
+            cursor_to $(( $startrow + $row + 1)) $(( $offset * $col + 1))
+            if [ $idx -eq $curr_idx ]; then
+                print_selected "$option"
+            else
+                print_option "$option"
+            fi
+            ((idx++))
+        done
+    }
+
+    # initially print empty new lines (scroll down if at bottom of screen)
+    for opt; do printf "\n"; done
+
+    # determine current screen position for overwriting the options
+    local return_value=$1
+    local lastrow=`get_cursor_row`
+    local lastcol=`get_cursor_col`
+    local startrow=$(($lastrow - $#))
+    local startcol=1
+    local lines=$( tput lines )
+    local cols=$( tput cols ) 
+    local colmax=$2
+    local offset=$(( $cols / $colmax ))
+
+    local size=$4
+    shift 4
+
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local active_row=0
+    local active_col=0
+    while true; do
+        print_options_multicol $active_col $active_row 
+        # user key control
+        case `key_input` in
+            enter)  break;;
+            up)     ((active_row--));
+                    if [ $active_row -lt 0 ]; then active_row=0; fi;;
+            down)   ((active_row++));
+                    if [ $active_row -ge $(( ${#options[@]} / $colmax ))  ]; then active_row=$(( ${#options[@]} / $colmax )); fi;;
+            left)     ((active_col=$active_col - 1));
+                    if [ $active_col -lt 0 ]; then active_col=0; fi;;
+            right)     ((active_col=$active_col + 1));
+                    if [ $active_col -ge $colmax ]; then active_col=$(( $colmax - 1 )) ; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    return $(( $active_col + $active_row * $colmax ))
+}
+
+diskpart () {
+echo -ne "
+------------------------------------------------------------------------
+    THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK
+    Please make sure you know what you are doing because
+    after formating your disk there is no way to get data back
+------------------------------------------------------------------------
+
+"
+
+PS3='
+Select the disk to install on: '
+options=($(lsblk -n --output TYPE,KNAME,SIZE | awk '$1=="disk"{print "/dev/"$2"|"$3}'))
+
+select_option $? 1 "${options[@]}"
+disk=${options[$?]%|*}
+
+echo -e "\n${disk%|*} selected \n"
+    set_option DISK ${disk%|*}
+
+drivessd
+}
+
+diskpart
+
 # format drives and make partitions
 lsblk
+sgdisk -Z ${DISK}
+sgdisk -a 2048 -o ${DISK}
+
+sgdisk -n 1::+4G --typecode=1:8200 ${DISK}
 if [ ! -d "/sys/firmware/efi" ]; then
-	sfdisk -n 1::+1G --typecode=1:ef02 
+	sgdisk -n 2::+1G --typecode=2:ef02 ${DISK}
 else
-	sfdisk -n 1::+1G --typecode=1:ef00
+	sgdisk -n 2::+1G --typecode=2:ef00 ${DISK}
 
-sfdisk -n 2::+ --typecode=2:8300
-sfdisk -n 3::+4G
+sgdisk -n 3::-0 --typecode=3:8300 ${DISK}
 
-if [ $INSTALL_TYPE = encrypted ]; then
-	cryptsetup luksFormat /dev/$partition2
-	cryptsetup open /dev/$partition2 cryptlvm
+fi
 
+if [ $INSTALL_TYPE == encrypted ]; then
+	cryptsetup luksFormat /dev/$partition3
+	cryptsetup open /dev/$partition3 cryptlvm
+fi
 # make filesystems
-mkfs.fat -F32 /dev/$partition1
-if [ $INSTALL_TYPE = encrypted ]; then
+mkswap /dev/$partition1
+mkfs.fat -F32 /dev/$partition2
+if [ $INSTALL_TYPE == encrypted ]; then
 	mkfs.ext4 /dev/mapper/cryptlvm
 else
-	mkfs.ext4 /dev/$partition2
+	mkfs.ext4 /dev/$partition3
 fi
-mkswap /dev/$partition3
+
 
 # mounting drives
-swapon /dev/$partition3
-if [ $INSTALL_TYPE = encrypted ]; then
+swapon /dev/$partition1
+if [ $INSTALL_TYPE == encrypted ]; then
 	mount /dev/mapper/cryptlvm /mnt
 else
-	mount /dev/$partition2 /mnt
+	mount /dev/$partition3 /mnt
 fi
 
 # mounting boot 
@@ -54,7 +191,7 @@ if [ -d "/sys/firmware/efi" ]; then
 else
 	mkdir /mnt/boot 				
 	mount /dev/$partition1 /mnt/boot 
-	
+fi
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
 #/etc/pacman.d/mirrorlist 
@@ -73,7 +210,7 @@ install "{system[@]}"
 
 if [ -d "/sys/firmware/efi" ]; then
 	basestrap /mnt efibootmgr # for efi systems
-	
+fi
 # todo: encrypted installation
 	#cryptsetup lvm2 lvm2-dinit
 
