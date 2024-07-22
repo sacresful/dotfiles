5359a8a53725baa6bc5afdf4336693350ef417f5
sacresful <57761430+sacresful@users.noreply.github.com>
2024-07-22 16:39:32 +0200

    Add files via upload

--- Artixstrap.sh
+++ Artixstrap.sh
@@ -197,7 +197,7 @@ sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 #			Setting up Mirrors
 #-------------------------------------------------------------------------
 
-pacman -Sy pacman-contrib
+pacman -Sy --noconfirm pacman-contrib
 echo "Generating mirror list"
 rankmirrors -n 10 -m 0.5 /etc/pacman.d/mirrorlist | grep -i '^Server' > /etc/pacman.d/mymirrorlist
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/artixmirrorlist
